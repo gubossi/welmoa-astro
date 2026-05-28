db0a4602611b81ce826716d7bb8a08aa941a4823
gubossi <gubossi@gmail.com>
2026-05-29 02:19:43 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import rehypeMermaid from 'rehype-mermaid';
 
 export default defineConfig({
   site: 'https://blog.welmoa.kr',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+
+  markdown: {
+    rehypePlugins: [rehypeMermaid]
+  }    
 });
