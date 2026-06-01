cd8d09fb76e2e599cdc024379aee8f8eee2cf722
gubossi <gubossi@gmail.com>
2026-06-01 13:29:13 +0900

    Create latest-posts.json.js

---
+++ src/pages/latest-posts.json.js
@@ -0,0 +1,26 @@
+import { getCollection } from 'astro:content';
+
+export async function GET() {
+  const posts = (await getCollection('blog'))
+    .filter((post) => !post.data.draft)
+    .sort(
+      (a, b) =>
+        new Date(b.data.pubDate).getTime() -
+        new Date(a.data.pubDate).getTime()
+    )
+    .slice(0, 3)
+    .map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      category: post.data.category,
+      pubDate: post.data.pubDate,
+      url: `https://blog.welmoa.kr/blog/${post.id}/`,
+    }));
+
+  return new Response(JSON.stringify(posts), {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
+}
